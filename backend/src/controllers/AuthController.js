87ed7b236254f6dd40d0198ec7b119457c411659
marcelobojikian <marcelo.bojikian@gmail.com>
2019-10-05 06:52:45 +0200

    Agregado el token

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -1,28 +1,56 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 module.exports = {
 
-    login(req, res) {
+    async authenticate(req, res) {
 
-        const { body, storeUser } = req
-        const user = body
+        const { storeUser } = req;
 
-        if (storeUser.has(user.mail)) {
+        try {
 
-            const userInfo = storeUser.get(user.mail)
-            const { password, invitedBy, requiredFirstStep } = userInfo
+            const user = req.body;
+            const mail = user.mail.split('.').join('\\.')
 
-            if (password == user.password) {
-                return res.json({ 
-                    status: "ok", 
-                    invitedBy, 
-                    requiredFirstStep })
-
-            } else {
-                return res.json({ status: "password invalido" })
+            if (!storeUser.has(mail)) {
+                return res.status(400).json({ error: "User already exists" });
             }
-        } else {
-            return res.json({ status: "usuario invalido" })
+
+            const userInfo = storeUser.get(mail)
+
+            const correct = await bcrypt.compare(user.password, userInfo.password);
+
+            if (!correct) {
+                return res.status(400).json({ error: "Invalid password" });
+            }
+
+            const { invitedBy, requiredFirstStep } = userInfo
+
+            return res.json({
+                mail: user.mail,
+                invitedBy,
+                requiredFirstStep,
+                token: jwt.sign({ id: user.mail }, "secret", { expiresIn: 86400 })
+            });
+
+        } catch (err) {
+            return res.status(400).json({ error: "User authentication failed" });
         }
 
+    },
+
+    async me(req, res) {
+        try {
+            const { storeUser, userId } = req;
+            const mail = userId.split('.').join('\\.')
+
+            const userInfo = storeUser.get(mail)
+
+            return res.json({ mail: userId, requiredFirstStep: userInfo.requiredFirstStep });
+        } catch (err) {
+            return res.status(400).json({ error: "Can't get user information" });
+        }
     }
+
 }
 
